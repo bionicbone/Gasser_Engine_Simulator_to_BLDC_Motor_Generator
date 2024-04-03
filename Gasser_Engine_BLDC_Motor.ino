f04a69b81975d64948e261c6c97d9b4206df1617
Kevin Guest <github@bionicbone.plus.com>
2024-04-03 15:16:34 +0100

    Initial PID added, but just pulsing so far

--- Gasser_Engine_BLDC_Motor.ino
+++ Gasser_Engine_BLDC_Motor.ino
@@ -7,18 +7,22 @@
 #include <PID_v1.h>
 #include <ESP32_ESC_High_Resolution_Driver.h>
 
-#define PIN_INPUT 0
-#define PIN_OUTPUT 3
-
 ESP32_ESC_HIGH_RESOLUTION_DRIVER ESC_Control = ESP32_ESC_HIGH_RESOLUTION_DRIVER();
-byte ESC_pin = 4;
-int ESC_Frequency = 50;
-byte RPM_pin = 14;
+const byte ESC_pin = 4;
+const int ESC_Frequency = 50;
+const byte RPM_pin = 14;
 int RPM_counter = 0;
 unsigned long RPM_timer = 0;
 
-void setup()
-{
+//Define PID control Variables we'll be connecting to
+double PID_setpoint, PID_input, PID_output;
+int PID_mappedOutput = 0;
+//Specify the links and initial tuning parameters
+double Kp = 2, Ki = 5, Kd = 1;
+PID myPID(&PID_input, &PID_output, &PID_setpoint, Kp, Ki, Kd, DIRECT);
+
+
+void setup() {
   Serial.begin(115200);
   while (!Serial && millis() < 5000);
   vTaskDelay(500);
@@ -41,27 +45,54 @@ void setup()
   pinMode(RPM_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(RPM_pin), RPM_ISR, RISING);
   RPM_timer = millis();
+
+  ESC_Control.ESC_set_us(1200);  //RPM: 1120 = 5k, 1200 = 10k, 1220 = 11.4k, 1240 = 12.2k 
+  delay(2000);
+
+  //initialize the PID variables we're linked to
+  PID_input = 0;        // 0 because the motor is not yet spinning.
+  PID_setpoint = 200;   // 12000 RPM / 60 = 200 Revolutions per Second
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
-
-  ESC_Control.ESC_set_us(1200);  //RPM: 1120 = 5k, 1200 = 10k, 1220 = 11.4k, 1240 = 12.2k 
-  if (RPM_counter > 100) {
+  // check RPM every second
+  // RPM_counter = 200 = 12000 RPM
+  if (millis() >= RPM_timer + 1000) {   
     detachInterrupt(digitalPinToInterrupt(RPM_pin));
     int finalRPM = (RPM_counter / ((float)(millis() - RPM_timer) / 1000)) * 60;
+    PID_input = RPM_counter;
     Serial.print("Final RPM Calculation = ");
     Serial.println(finalRPM);
+    
+    // PID Calculation
+    myPID.Compute();
+    PID_mappedOutput = map(PID_output, 0, 255, 1000, 2000);
+    Serial.print("PID_output = ");
+    Serial.println(PID_output);
+    Serial.print("PID_input = ");
+    Serial.println(PID_input);
+    Serial.print("PID_mappedOutput = ");
+    Serial.println(PID_mappedOutput);
+    ESC_Control.ESC_set_us(PID_mappedOutput);
+    
     RPM_timer = millis();
     RPM_counter = 0;
     attachInterrupt(digitalPinToInterrupt(RPM_pin), RPM_ISR, RISING);
   }
+  
+
+
+  
+  
   /*
     Example COde for ESP32_ESC_HIGH_RESOLUTION_DRIVER
     0-25% example, limited to 25% because it is not good to spin a motor with zero load at full speed.
     Alter as necessary, with caution, watch out for flying propellers or motor cans pulling off the main motor body !!
     Ask me how I know, USE WITH CAUTION !!!
   */
-
 
   //// increment by us for RC ESC for Max Resolution of 1000 steps
   //for (uint16_t i = 1000; i < 1250; i++) {
